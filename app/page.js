b5085351e4d6655283b6ee79fbbdcd9e62f00231
diviebisike <divine.ebisike@duke.edu>
2024-09-03 18:44:53 -0400

    very barebones homepage and navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import styles from "./page.module.css";
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
-      Homepage
+      <div className={styles.centeredText}>
+      <h1>Let's Meet!</h1>
+      </div>
+      
     </div>
   );
 }
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styles from './Navbar.module.css'; 
 
-const Navbar = () =>  {
+const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
-    )
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <img src="/images/LogoTransparent.png" alt="Logo" /> 
+            </div>
+            <div className={styles.navLinks}>
+                <a href="#">How It Works</a>
+                <a href="#">Sign Up</a>
+                <a href="#">Log In</a>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
